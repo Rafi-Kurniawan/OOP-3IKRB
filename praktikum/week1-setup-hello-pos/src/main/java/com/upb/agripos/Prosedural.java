6deb4d4d2270c1685dbf3d855eb3a7a8fc3e11d4
Rafi-Kurniawan <kurniawanrafi421@gmail.com>
2025-10-09 11:33:06 +0700

    Update Prosedural.java

--- praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/Prosedural.java
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/Prosedural.java
@@ -1,21 +1,11 @@
-// HelloProcedural.java
-public class HelloProcedural {
-   public static void main(String[] args) {
-      String nim = "240202878";
-      String nama = "Rafi Kurniawan";
-      String[] produk = {"Beras", "Pupuk", "Benih"};
-      int[] harga = {10000, 15000, 12000};
-      int total = 0;
+public class HelloProsedural {
+    public static void main(String[] args) {
+        String name = "Rafi Kurniawan";
+        String nim = "123456789";
+        greet(name, nim);
+    }
 
-      System.out.println("Hello POS World");
-      System.out.println("NIM: " + nim + ", Nama: " + nama);
-      System.out.println("Daftar Produk:");
-
-      for (int i = 0; i < produk.length; i++) {
-         System.out.println("- " + produk[i] + ": " + harga[i]);
-         total += harga[i];
-      }
-
-      System.out.println("Total harga semua produk: " + total);
-   }
+    static void greet(String name, String nim) {
+        System.out.println("Hello World, I am " + name + "-" + nim);
+    }
 }
