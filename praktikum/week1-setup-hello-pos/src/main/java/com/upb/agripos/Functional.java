d391d2cc8de64f28303aec1a296cce91d6c3e70d
Rafi-Kurniawan <kurniawanrafi421@gmail.com>
2025-10-09 11:34:37 +0700

    Update Functional.java

--- praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/Functional.java
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/Functional.java
@@ -1,26 +1,10 @@
-// HelloFunctional.java
-import java.util.*;
-import java.util.stream.*;
+import java.util.function.BiFunction;
 
 public class HelloFunctional {
-   public static void main(String[] args) {
-      String nim = "240202878";
-      String nama = "Rafi Kurniawan";
+    public static void main(String[] args) {
+        BiFunction<String, String, String> greet = (name, nim) ->
+            "Hello World, I am " + name + "-" + nim;
 
-      List<String> produk = Arrays.asList("Beras", "Pupuk", "Benih");
-      List<Integer> harga = Arrays.asList(25000, 40000, 50000);
-
-      System.out.println("Hello POS World");
-      System.out.println("NIM: " + nim + ", Nama: " + nama);
-      System.out.println("Daftar Produk:");
-
-      // Menggunakan lambda dan stream untuk menampilkan produk & harga
-      IntStream.range(0, produk.size())
-         .forEach(i -> System.out.println("- " + produk.get(i) + ": " + harga.get(i)));
-
-      // Menggunakan stream untuk menghitung total harga
-      int total = harga.stream().mapToInt(Integer::intValue).sum();
-
-      System.out.println("Total harga semua produk: " + total);
-   }
+        System.out.println(greet.apply("Rafi Kurniawan", "123456789"));
+    }
 }
