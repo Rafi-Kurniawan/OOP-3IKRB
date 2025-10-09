947d9372fd4536530b7389e7280a0a0335603327
Rafi-Kurniawan <kurniawanrafi421@gmail.com>
2025-10-09 11:33:50 +0700

    Update OOP.java

--- praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/OOP.java
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/OOP.java
@@ -1,42 +1,20 @@
-// HelloOOP.java
+class HelloWorld {
+    private String name;
+    private String nim;
 
-// Kelas Produk merepresentasikan satu entitas "produk"
-class Produk {
-   String nama;
-   int harga;
+    public HelloWorld(String name, String nim) {
+        this.name = name;
+        this.nim = nim;
+    }
 
-   // Konstruktor untuk membuat objek Produk baru
-   Produk(String nama, int harga) {
-      this.nama = nama;
-      this.harga = harga;
-   }
+    public void greet() {
+        System.out.println("Hello World, I am " + name + "-" + nim);
+    }
 }
 
 public class HelloOOP {
-   public static void main(String[] args) {
-      // Data identitas mahasiswa
-      String nim = "240202878";
-      String namaMhs = "Rafi Kurniawan";
-
-      // Membuat array berisi beberapa objek Produk
-      Produk[] daftar = {
-         new Produk("Beras", 24000),
-         new Produk("Pupuk", 66000),
-         new Produk("Benih", 10000)
-      };
-
-      int total = 0;
-
-      System.out.println("Hello POS World");
-      System.out.println("NIM: " + nim + ", Nama: " + namaMhs);
-      System.out.println("Daftar Produk:");
-
-      // Menampilkan semua produk menggunakan perulangan berbasis objek
-      for (Produk p : daftar) {
-         System.out.println("- " + p.nama + ": " + p.harga);
-         total += p.harga;
-      }
-
-      System.out.println("Total harga semua produk: " + total);
-   }
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld("Rafi Kurniawan", "123456789");
+        hello.greet();
+    }
 }
